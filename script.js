6c1bc4ab556af0c7681fd2ec8c97e399bf37940d
Davi Lopes <dalomester@gmail.com>
2017-06-29 12:08:47 -0300

    Site

---
+++ script.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+
+  //drop down menu
+  //$(window).scroll(function(){
+    //var scrollPos = $(this).scrollTop();
+
+  //  if(scrollPos > 80){
+      //$(".barMenu").show();
+    //  $(".dropdownMenu").show();
+      $(".fa-times").click(function(){
+        $(".dropdownMenu").addClass("escondeMenu");
+        $(".barMenu").addClass("opacidadeUm");
+      });
+
+      $(".barMenu").click(function(){
+        $(".dropdownMenu").removeClass("escondeMenu");
+        $(".barMenu").removeClass("opacidadeUm");
+
+
+      });
+
+    //}else{
+    //  $(".barMenu").hide();
+      //$(".dropdownMenu").hide();
+
+  //});
+
+  //scroll down
+  $("#topMenuHome").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#imgHTML").offset().top
+    }, 1000);
+  });
+
+  $("#topMenuAbout").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#About").offset().top
+    }, 1000);
+  });
+
+  $("#topMenuResume").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Resume").offset().top
+    }, 1000);
+  });
+
+  $("#topMenuSkills").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Skills").offset().top
+    }, 1000);
+  });
+
+  $("#topMenuContact").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#Contact").offset().top
+    }, 1000);
+  });
+
+  //RESPONSIVE
+  $(window).resize(function() {
+    if ($(window).width() < 768) {
+      $("#div1").hide();
+      $(".barMenu").hide();
+    }
+    else {
+      $("#div1").show();
+      $(".barMenu").show();
+    }
+  });
+
+
+});
